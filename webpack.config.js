0e67abd7e4d5352d257205d16cdca11c83a6fa30
bijink <kbijin528@gmail.com>
2022-04-07 19:43:52 +0530

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+   mode: "development",
+   entry: path.resolve(__dirname, './src/index.jsx'),
+   devtool: "eval-source-map",
+   module: {
+      rules: [
+         {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: ['babel-loader'],
+         },
+         {
+            test: /\.(css)$/,
+            use: ['style-loader', 'css-loader']
+         },
+      ],
+   },
+   resolve: {
+      extensions: ['*', '.js', '.jsx'],
+   },
+   output: {
+      path: path.resolve(__dirname, './build'),
+      filename: 'bundle.js',
+   },
+   plugins: [
+      new HtmlWebpackPlugin({
+         template: path.resolve(__dirname, './public/index.html'),
+      }),
+   ]
+};
